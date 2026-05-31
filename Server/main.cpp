49bf9a07629f040b5009bc0414fc3de2ca7e3fdd
Pelivan <Pelivan@DESKTOP-29KL5UV>
2026-05-31 14:41:39 +0300

    TODO: Chat

--- Server/main.cpp
+++ Server/main.cpp
@@ -8,29 +8,36 @@
 #include<winsock2.h>
 #include<WS2tcpip.h>
 #include<iphlpapi.h>
+
+#include<FormatLastError.h>
 using namespace std;
 
 #pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "FormatLastError.lib")
 
 #define PORT	"27015"
-#define BUFFER_LENGHT	1500
-#define MAX_CONNECTIONS	5
+#define BUFFER_LENGTH	1500
+#define MAX_CONNECTIONS	 5
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "SERVER" << endl;
-	//1)
+	DWORD dwError = 0;
+	CHAR szError[256] = {};
+	//1) Init WinSOCK:
 	WSADATA wsaData;
 	int iResult;
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	dwError = WSAGetLastError();
 	if (iResult != 0)
 	{
-		cout << "WSASturtup failed: " << iResult << endl;
+		cout << FormatLastError(dwError, szError) << endl;
+		cout << "WSAStartup failed: " << iResult << endl;
 		return;
 	}
 
-	//2) Параметры подключения
+	//2) Параметры подключения:
 	addrinfo hints;
 	addrinfo* result;
 	ZeroMemory(&hints, sizeof(hints));
@@ -40,69 +47,79 @@ void main()
 	hints.ai_flags = AI_PASSIVE;
 
 	iResult = getaddrinfo(NULL, PORT, &hints, &result);
+	dwError = WSAGetLastError();
 	if (iResult != 0)
 	{
+		cout << FormatLastError(dwError, szError) << endl;
 		cout << "getaddrinfo() failed: " << iResult << endl;
 		WSACleanup();
 		return;
 	}
 
-	//3) Создаем сокет для сервера который он будет постоянно слушать
+	//3) Создаем сокет для сервера, который он будет постоянно слушать "LISTENING":
 	SOCKET listen_socket =
 		socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	dwError = WSAGetLastError();
 	if (listen_socket == INVALID_SOCKET)
 	{
-		cout << "Listen socket error: " << WSAGetLastError ()<< endl;
+		cout << FormatLastError(dwError, szError) << endl;
+		cout << "Listen socket error: " << WSAGetLastError() << endl;
 		freeaddrinfo(result);
 		WSACleanup();
 		return;
-
 	}
-	//4) Bind socket
+
+	//4) Bind socket:
 	iResult = bind(listen_socket, result->ai_addr, result->ai_addrlen);
+	dwError = WSAGetLastError();
 	if (iResult == SOCKET_ERROR)
 	{
+		cout << FormatLastError(dwError, szError) << endl;
 		cout << "Bind failed with error: " << WSAGetLastError() << endl;
 		closesocket(listen_socket);
 		freeaddrinfo(result);
 		WSACleanup();
 		return;
-
 	}
 	freeaddrinfo(result);
 
-	//5) Запускаем прослушивание сокета
+	//5) Запускаем прослушивание сокета:
 	if (listen(listen_socket, MAX_CONNECTIONS) == SOCKET_ERROR)
 	{
+		dwError = WSAGetLastError();
+		cout << FormatLastError(dwError, szError) << endl;
 		cout << "Listen failed with error: " << WSAGetLastError() << endl;
 		closesocket(listen_socket);
 		freeaddrinfo(result);
 		WSACleanup();
 		return;
-
 	}
 
-	//6) Обработка запросов от клиентов
-
+	//6) Обработка соединений от клиентов:
 	SOCKET client_socket = accept(listen_socket, NULL, NULL);
+	dwError = WSAGetLastError();
 	if (client_socket == INVALID_SOCKET)
 	{
+		cout << FormatLastError(dwError, szError) << endl;
 		cout << "Accept failed with error: " << WSAGetLastError() << endl;
-		
 	}
-	//7)
-	CHAR recvbuffer[BUFFER_LENGHT] = {};
-	CHAR sendbuffer[BUFFER_LENGHT] = {};
+
+	//7) Получение и отправка данных:
+	CHAR recvbuffer[BUFFER_LENGTH] = {};
+	CHAR sendbuffer[BUFFER_LENGTH] = {};
 	INT iSendResult = 0;
 	do
 	{
-		iResult = recv(client_socket, recvbuffer, BUFFER_LENGHT, 0);
+		iResult = recv(client_socket, recvbuffer, BUFFER_LENGTH, 0);
+		dwError = WSAGetLastError();
 		if (iResult > 0)
 		{
 			cout << recvbuffer << "(" << strlen(recvbuffer) << " Bytes)" << endl;
 			iSendResult = send(client_socket, recvbuffer, strlen(recvbuffer), 0);
+			dwError = WSAGetLastError();
 			if (iSendResult == SOCKET_ERROR)
 			{
+				cout << FormatLastError(dwError, szError) << endl;
 				cout << "Send failed with error: " << WSAGetLastError() << endl;
 				closesocket(client_socket);
 			}
@@ -111,19 +128,21 @@ void main()
 		else if (iResult == 0) cout << "Connection closing..." << endl;
 		else
 		{
+			cout << FormatLastError(dwError, szError) << endl;
 			cout << "Receive failed with error: " << WSAGetLastError() << endl;
 			closesocket(client_socket);
 		}
 	} while (iResult > 0);
 
 	iResult = shutdown(client_socket, SD_BOTH);
-	if (iResult == SOCKET_ERROR)cout << "Client shutdown failed with error: " << WSAGetLastError() << endl;
+	dwError = WSAGetLastError();
+	if (iResult == SOCKET_ERROR)cout << "Client shutdown failed with  " << FormatLastError(dwError, szError) << endl;
 
 	iResult = shutdown(listen_socket, SD_BOTH);
-	if (iResult == SOCKET_ERROR)cout << "Listen shutdown failed with error: " << WSAGetLastError() << endl;
+	dwError = WSAGetLastError();
+	if (iResult == SOCKET_ERROR)cout << "Server shutdown failed with  " << FormatLastError(dwError, szError) << endl;
 
 	closesocket(client_socket);
 	closesocket(listen_socket);
-
 	WSACleanup();
 }
